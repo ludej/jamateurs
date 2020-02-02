245a80ba06a90dd4db55ef5b683dbd7f5faa87ae
Matej Duras <matej.duras@sap.com>
2020-02-02 12:27:46 +0100

    info screen

---
+++ info.lua
@@ -0,0 +1,90 @@
+-----------------------------------------------------------------------------------------
+--
+-- info.lua
+--
+--------------------------------------------------------------------------
+
+--require("mobdebug").start()
+local composer = require( "composer" )
+local scene = composer.newScene()
+
+
+local function leave()
+    audio.stop()
+    composer.gotoScene("level4")
+end
+
+--------------------------------------------
+
+function scene:create( event )
+
+	-- Called when the scene's view does not exist.
+	--
+	-- INSERT code here to initialize the scene
+	-- e.g. add display objects to 'sceneGroup', add touch listeners, etc.
+
+	local sceneGroup = self.view
+    background = display.newImageRect(sceneGroup, "Images/Scene/UI/intro.png",1920, 1080)
+    background.x = display.contentWidth*0.5
+    background.y = display.contentHeight*0.5
+    background:toBack()
+	sceneGroup:insert( background )
+
+end
+
+
+function scene:show( event )
+	local sceneGroup = self.view
+	local phase = event.phase
+
+	if phase == "will" then
+		-- Called when the scene is still off screen and is about to move on screen
+	elseif phase == "did" then
+		-- Called when the scene is now on screen
+		--
+		-- INSERT code here to make the scene come alive
+
+        timer.performWithDelay( 5000, leave, 1 )
+
+	end
+end
+
+function scene:hide( event )
+	local sceneGroup = self.view
+
+	local phase = event.phase
+
+	if event.phase == "will" then
+		-- Called when the scene is on screen and is about to move off screen
+		--
+		-- INSERT code here to pause the scene
+		-- e.g. stop timers, stop animation, unload sounds, etc.)
+
+	elseif phase == "did" then
+		-- Called when the scene is now off screen
+	end
+
+end
+
+function scene:destroy( event )
+
+	-- Called prior to the removal of scene's "view" (sceneGroup)
+	--
+	-- INSERT code here to cleanup the scene
+	-- e.g. remove display objects, remove touch listeners, save state, etc.
+	local sceneGroup = self.view
+
+
+end
+
+---------------------------------------------------------------------------------
+
+-- Listener setup
+scene:addEventListener( "create", scene )
+scene:addEventListener( "show", scene )
+scene:addEventListener( "hide", scene )
+scene:addEventListener( "destroy", scene )
+
+-----------------------------------------------------------------------------------------
+
+return scene
