8929dd3fedfdf4ccaef09457549cf3a1ba091286
Jan Vrtiska <jv673808@broadcom.net>
2020-02-02 01:25:08 +0100

    Game background

--- level4.lua
+++ level4.lua
@@ -11,7 +11,6 @@ local physics = require ("physics")
 local scene = composer.newScene()
 local sceneGroup
 
-
 local flames
 local arnold
 local caravan
@@ -38,7 +37,7 @@ local platforms = {}
 local platformCount = 0
 local enemies = {}
 local enemiesCount = 0
-local gameOverScreen, gameoverBackground
+local gameBackground,gameOverScreen, gameoverBackground
 local countDownSecondsText, countDownText, levelLegendText, levelCounterText
 
 
@@ -560,7 +559,7 @@ function scene:create( event )
 	physics.setGravity(0, 20)
 	--physics.pause()
 
-  physics.setDrawMode("hybrid") -- shows the physics box around the object
+ -- physics.setDrawMode("hybrid") -- shows the physics box around the object
 
 
 
@@ -568,13 +567,8 @@ function scene:create( event )
 	-- the physical screen will likely be a different shape than our defined content area
 	-- since we are going to position the background from it's top, left corner, draw the
 	-- background at the real top, left corner.
-	local background = display.newRect( display.screenOriginX, display.screenOriginY, screenW, screenH )
-	background.anchorX = 0
-	background.anchorY = 0
-	background:setFillColor( .5 )
 
-
-    lever = display.newImageRect( "Images/Scene/lever.png", 50, 50)
+  lever = display.newImageRect( "Images/Scene/lever.png", 50, 50)
 	lever.anchorX = 0
 	lever.anchorY = 1
 	lever.x, lever.y = 0, 225
@@ -627,8 +621,8 @@ function scene:create( event )
 
     --sendArnie()
 
-	-- all display objects must be inserted into group
-    sceneGroup:insert( background )
+
+    
     sceneGroup:insert( entrancePortal )
     sceneGroup:insert( exit )
 	sceneGroup:insert( grass)
@@ -654,7 +648,7 @@ local function teleportIn()
   transition.fadeIn(entrancePortal, { time=300, delay=500, onComplete=function() audio.play(utils.sounds["teleport"]) end} )
   transition.fadeIn(arnold, {
     time=500, delay=800, onComplete=function()
-    timer.resume(gameLoopTimer)
+    --timer.resume(gameLoopTimer)
     arnold:setSequence("running")
     arnold:play()
     arnoldMover(1)
@@ -698,6 +692,11 @@ end
 function scene:show( event )
 	sceneGroup = self.view
 	local phase = event.phase
+  
+  gameBackground = display.newImageRect(sceneGroup, "Images/Scene/background/bg_all.png",1920, 1080)
+  gameBackground.x = display.contentWidth*0.5
+  gameBackground.y = display.contentHeight*0.5
+  gameBackground:toBack()
 
 	if phase == "will" then
 		-- Called when the scene is still off screen and is about to move on screen
@@ -730,7 +729,7 @@ function scene:show( event )
 
     physics.start()
     createEnemy(1400,900,"enemy", -1)
-
+    sendArnie()
 
 	end
 end
